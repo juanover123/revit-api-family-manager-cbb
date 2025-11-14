ab04a208ba155e387bd30efc3f1fa48c695ec921
juanover123 <g.juan.garcia.1997@gmail.com>
2025-11-14 19:24:36 -0300

    Family Manager dockable pane boilerplate added

---
+++ src/cbb.core/Commands/AnnotatePanel/FamilyManager/FamilyManagerHideCommand.cs
@@ -0,0 +1,42 @@
+﻿namespace cbb.core
+{
+    using Autodesk.Revit.DB;
+    using Autodesk.Revit.UI;
+
+    /// <summary>
+    /// Hide Family Manager dockable pane.
+    /// </summary>
+    /// <seealso cref="Autodesk.Revit.UI.IExternalCommand" />"/>
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    public class FamilyManagerHideCommand : IExternalCommand
+    {
+        #region public methods
+
+        /// <summary>
+        /// Executes the specified command data.
+        /// </summary>
+        /// <param name="commandData">The command data.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="elements">The elements.</param>
+        /// <returns></returns>
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Family Manager", "Hide Family Manager dockable pane command executed.");
+
+            return Result.Succeeded;
+        }
+
+        /// <summary>
+        /// Gets the full namespace path to this command.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetPath()
+        {
+            // Return constructed namespace path.
+            return typeof(FamilyManagerHideCommand).Namespace + "." + nameof(FamilyManagerHideCommand);
+        }
+
+        #endregion
+    }
+}
--- src/cbb/SetupInterface.cs
+++ src/cbb/SetupInterface.cs
@@ -29,8 +29,11 @@
             string tabName = "JuanoverTestBox";
             app.CreateRibbonTab(tabName);
 
-            // Create the annotate commands panel.
+            // Create the ribbon panels.
             var annotateCommandsPanel = app.CreateRibbonPanel(tabName, "Annotation Commands");
+            var familyManagerCommandsPanel = app.CreateRibbonPanel(tabName, "Family Manager Commands");
+
+            #region annotate panel
 
             // Populate button data model.
             var TagWallLayersButtonData = new RevitPushButtonDataModel
@@ -39,13 +42,45 @@
                 Panel = annotateCommandsPanel,
                 ToolTip = "This is some sample tooltip text, replace it with real one later,...",
                 CommandNamespacePath = TagWallLayersCommand.GetPath(),
-                IconImageName = "wallTag_icon_32x32.png",
+                IconImageName = "wallTag_icon_96x96.png",
                 TooltipImageName = "wallTag_tooltip_320x320.png",
-                SmallImageName = "wallTag_icon_16x16.png"
+                SmallImageName = "wallTag_icon_32x32.png"
             };
 
             // Create button from provided data.
             var TagWallLayersButton = RevitPushButton.Create(TagWallLayersButtonData);
+
+            #endregion
+
+            #region family manager
+
+            var familyManagerShowButtonData = new RevitPushButtonDataModel
+            {
+                Label = "Show Family\nManager",
+                Panel = familyManagerCommandsPanel,
+                ToolTip = "This is some sample tooltip text, replace it with real one later,...",
+                CommandNamespacePath = FamilyManagerShowCommand.GetPath(),
+                IconImageName = "familyManagerShow_icon_96x96.png",
+                TooltipImageName = "familyManagerShow_tooltip_320x320.png",
+                SmallImageName = "familyManagerShow_icon_32x32.png"
+            };
+
+            var familyManagerShowButton = RevitPushButton.Create(familyManagerShowButtonData);
+
+            var familyManagerHideButtonData = new RevitPushButtonDataModel
+            {
+                Label = "Hide Family\nManager",
+                Panel = familyManagerCommandsPanel,
+                ToolTip = "This is some sample tooltip text, replace it with real one later,...",
+                CommandNamespacePath = FamilyManagerHideCommand.GetPath(),
+                IconImageName = "familyManagerHide_icon_96x96.png",
+                TooltipImageName = "familyManagerHide_tooltip_320x320.png",
+                SmallImageName = "familyManagerHide_icon_32x32.png"
+            };
+
+            var familyManagerHideButton = RevitPushButton.Create(familyManagerHideButtonData);
+
+            #endregion
         }
 
         #endregion
